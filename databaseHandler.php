c83ed209e44e137a7dbef1468434d64d973bc03c
Lachlan Brown <lachlan@lachlanb.com>
2017-10-15 19:09:14 +1100

    v0.12
    Looking real nice now. Don't expect this commit to work, this is just a
    checkpoint for me and contains no information on executing its newly
    added functions

--- databaseHandler.php
+++ databaseHandler.php
@@ -2,6 +2,12 @@
 
 session_start();
 
+
+//DEBUG FAKE VALUES
+//$_POST['action'] = "read";
+//$_POST['type'] = "journal";
+
+
 $conn = new mysqli("localhost", "root", "mysql", "sdp");
 
 if ($conn->connect_error) {
@@ -34,16 +40,14 @@ else if($_POST['action'] == "read"){
 		if($_POST['username'] == "admin"){
 			echo "admin";
 			$_SESSION['currentUser'] = "admin";
-		}
-		else if ($users=$conn->query($sql)){
+		} else if ($users=$conn->query($sql)){
 			$sql="SELECT * FROM users WHERE username LIKE '" . $_POST['username'] . "'";
 			while ($user=mysqli_fetch_row($users)){
 				echo $user[0];
 				$_SESSION['currentUser'] = $user[0];
 			}
 		}
-	}
-	else if($_POST['type'] == "journal"){
+	} else if($_POST['type'] == "journal"){
 		if(!isset($_SESSION['currentUser'])){
             echo "LOGIN FIRST";
         } else {
@@ -54,8 +58,34 @@ else if($_POST['action'] == "read"){
 			}
 			if ($journals=$conn->query($sql)){
 				while ($journal=mysqli_fetch_row($journals)){
-					printf ("(Title) %s (Creator) %s", $journal[0], $journal[1]);
-					$_SESSION['currentUser'] = $journal[0];
+					printf ("%s|||%s|| ||", $journal[0], $journal[1]);
+				}
+
+			}
+		} 
+	} else if($_POST['type'] == "user"){
+		$sql="SELECT * FROM users";
+		if ($users=$conn->query($sql)){
+			while ($user=mysqli_fetch_row($users)){
+				echo $user[0];
+				echo "|||";
+				echo $user[1];
+				echo "|| ||";
+			}
+		}
+	} else if($_POST['type'] == "usersdsd"){
+		if(!isset($_SESSION['currentUser'])){
+            echo "LOGIN FIRST";
+        } else {
+        	//if($_SESSION['currentUser'] == "admin"){
+    		$sql="SELECT * FROM users";
+        	//} else {
+			//	$sql="SELECT * FROM journals WHERE creator LIKE '" . $_SESSION['currentUser'] . "'";
+			//}
+			if ($users=$conn->query($sql)){
+				while ($users=mysqli_fetch_row($users)){
+					printf ($user[0], $user[1]);
+					//$_SESSION['currentUser'] = $journal[0];
 				}
 			}
 		}
